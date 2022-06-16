30bbd4206578f65016cfe48a041b5f838e645aa6
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-06-16 19:12:15 +0900

    add:Icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,13 @@ import React  from "react"
 import Toolbar from '@mui/material/Toolbar'
 import { styled } from "@mui/system"
 import { Button } from "@mui/material"
+
 interface NavbarProps {
   home: any
   about: any
   projects: any
   skills: any
+  news: any
   contact: any
 }
 
@@ -19,10 +21,11 @@ const NavbarButton = styled(Button)({
       }
 })
 
-const Navbar:React.FC<NavbarProps> = ({home, about, projects, skills, contact}) => {
+const Navbar:React.FC<NavbarProps> = ({home, about, projects, skills, contact,news}) => {
     const scrollAbout = () => about.current.scrollIntoView({behavior: "smooth"})
     const scrollProjects = () => projects.current.scrollIntoView({behavior: "smooth"})
-    const scrollSkills = () => skills.current.scrollIntoView({behavior: "smooth"})
+    const scrollSkills = () => skills.current.scrollIntoView({ behavior: "smooth" })
+    const scrollNews = () => news.current.scrollIntoView({ behavior: "smooth" })
     const scrollContact = () => contact.current.scrollIntoView({behavior: "smooth"})
 
   return (
@@ -35,7 +38,9 @@ const Navbar:React.FC<NavbarProps> = ({home, about, projects, skills, contact}) 
           <NavbarButton onClick={scrollAbout}>About</NavbarButton>
           <NavbarButton onClick={scrollSkills}>Skills</NavbarButton>
           <NavbarButton onClick={scrollProjects}>Projects</NavbarButton>
+          <NavbarButton onClick={scrollProjects}>News</NavbarButton>
           <NavbarButton onClick={scrollContact}>Contact</NavbarButton>
+      
         </Toolbar>
   );
 }
