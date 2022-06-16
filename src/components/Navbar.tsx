c5d4fd17745cd335dfda73dc031ad5aa34cf703e
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-06-17 04:13:06 +0900

    add:burger

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
 import React  from "react"
 import Toolbar from '@mui/material/Toolbar'
+import useMedia from "use-media";
 import { styled } from "@mui/system"
 import { Button } from "@mui/material"
+import MediaQuery from "react-responsive";
+import { slide as Menu } from 'react-burger-menu'
+import './styles/style.css'
 
 interface NavbarProps {
   home: any
@@ -12,6 +16,7 @@ interface NavbarProps {
   contact: any
 }
 
+// const isWide = useMedia({ minWidth: "1000px" });
 
 const NavbarButton = styled(Button)({
       color: '#c29334',
@@ -33,14 +38,25 @@ const Navbar:React.FC<NavbarProps> = ({home, about, projects, skills, contact,ne
         sx={{
           justifyContent: "center"
         }}
-        >
-          <NavbarButton>Home</NavbarButton>
-          <NavbarButton onClick={scrollAbout}>About</NavbarButton>
-          <NavbarButton onClick={scrollSkills}>Skills</NavbarButton>
-          <NavbarButton onClick={scrollProjects}>Projects</NavbarButton>
-          <NavbarButton onClick={scrollProjects}>News</NavbarButton>
-          <NavbarButton onClick={scrollContact}>Contact</NavbarButton>
-      
+    >
+          <MediaQuery query="(min-width: 594px)" >
+            <NavbarButton>Home</NavbarButton>
+            <NavbarButton onClick={scrollAbout}>About</NavbarButton>
+            <NavbarButton onClick={scrollSkills}>Skills</NavbarButton>
+            <NavbarButton onClick={scrollProjects}>Projects</NavbarButton>
+            <NavbarButton onClick={scrollNews}>News</NavbarButton>
+            <NavbarButton onClick={scrollContact}>Contact</NavbarButton>
+        </MediaQuery>
+        <MediaQuery query="(max-width: 593px)">
+          <Menu>
+            <NavbarButton>Home</NavbarButton>
+            <NavbarButton onClick={scrollAbout}>About</NavbarButton>
+            <NavbarButton onClick={scrollSkills}>Skills</NavbarButton>
+            <NavbarButton onClick={scrollProjects}>Projects</NavbarButton>
+            <NavbarButton onClick={scrollNews}>News</NavbarButton>
+            <NavbarButton onClick={scrollContact}>Contact</NavbarButton>
+          </Menu>
+        </MediaQuery>
         </Toolbar>
   );
 }
