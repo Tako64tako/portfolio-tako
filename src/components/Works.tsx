bc59ae99e747052b4418d3f21b4ad72c124ad242
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-07-14 19:37:44 +0900

    add:geekcamp_vol.5

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -8,14 +8,15 @@ import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import { Button, CardActionArea, CardActions } from '@mui/material'
 import p1 from '../styles/assets/geekcamp.jpg'
-import p2 from '../styles/assets/koukaten.png'
+import p2 from '../styles/assets/twitter.jpg'
 import p3 from '../styles/assets/ctos.png'
 import p4 from '../styles/assets/othello.png'
+import p5 from '../styles/assets/koukaten.png'
 
 const Works: React.FC = () => {
     return (
         <section>
-              <GlobalBoxWrap sx={{textAlign: "center", marginTop: "10rem", marginBottom: "5rem"}}>
+            <GlobalBoxWrap sx={{textAlign: "center", marginTop: "10rem", marginBottom: "5rem"}}>
                 <GlobalSubTitle>Projects</GlobalSubTitle>
                     <Grid container alignItems="stretch">
                         <Grid item xs={6} md={4}>
@@ -24,16 +25,16 @@ const Works: React.FC = () => {
                                 <CardActionArea>
                                     <CardMedia
                                     component="img"
-                                    height='auto'
+                                    height='190px'
                                     image={p1}
                                     alt="project 1"
                                     />
                                     <CardContent>
                                     <Typography gutterBottom variant="h5" component="div" sx={{fontWeight: 600}}>
-                                        A warehouse where people can see
+                                        Can be seen by people
                                     </Typography>
                                     <Typography variant="body2" color="text.secondary">
-                                        Creation of mobile apps and web apps that detect the number of people in a room with Ibeacon.It was a three-person team in a two-day hackthon.I am in charge of sending and receiving Ibeacon, registering data in Firebase, and developing the backend.geekcamp effort award.
+                                        Creation of mobile apps and web apps that detect the number of people in a room with Ibeacon.It was a three-person team in a two-day hackthon.I am in charge of sending and receiving Ibeacon, registering data in Firebase, and developing the backend.geekcamp_vol.3 effort award.
                                     </Typography>
                                     </CardContent>
                                 </CardActionArea>
@@ -52,16 +53,16 @@ const Works: React.FC = () => {
                                     <CardActionArea>
                                         <CardMedia
                                         component="img"
-                                        height='auto'
+                                        height='190px'
                                         image={p2}
                                         alt="project 2"
                                         />
                                         <CardContent>
                                         <Typography gutterBottom variant="h5" component="div" sx={{fontWeight: 600}}>
-                                          The number of people at the Sys Lab
+                                            Hiding and watching Twitter
                                         </Typography>
                                         <Typography variant="body2" color="text.secondary">
-                                          It took 5 people to create a web application that reads the number of people from the camera to the club room over half a year.Responsible for backend development and deployment.University festival highest award work.
+                                            We made an extension. It was a three-person team in a two-day hackthon. I did a backend development that uses Twitter api and flask to pass information to the server.. geekcamp_vol.5 effort award.
                                         </Typography>
                                         </CardContent>
                                     </CardActionArea>
@@ -81,13 +82,13 @@ const Works: React.FC = () => {
                                     <CardActionArea>
                                         <CardMedia
                                         component="img"
-                                        height='auto'
+                                        height='190px'
                                         image={p3}
                                         alt="project 3"
                                         />
                                         <CardContent>
                                         <Typography gutterBottom variant="h5" component="div" sx={{fontWeight: 600}}>
-                                          Online Try On Service<br />
+                                            Online Try On Service<br />
                                         </Typography>
                                         <Typography variant="body2" color="text.secondary">
                                             A service that allows you to change clothes on the Web was created by 5 people over half a year.Responsible for database definition, environment construction, and backend development.Second place work in the lecture.<br/>
@@ -113,9 +114,9 @@ const Works: React.FC = () => {
                                 <CardActionArea>
                                     <CardMedia
                                     component="img"
-                                    height='auto'
+                                    height='190px'
                                     image={p4}
-                                    alt="project 1"
+                                    alt="project 4"
                                     />
                                     <CardContent>
                                     <Typography gutterBottom variant="h5" component="div" sx={{fontWeight: 600}}>
@@ -135,28 +136,28 @@ const Works: React.FC = () => {
                             </Card>
                             </Box>
                         </Grid>
-                        {/* <Grid item xs={6} md={4}>
+                        <Grid item xs={6} md={4}>
                             <Box>
                                 <Card sx={{ maxWidth: 345 }}>
                                     <CardActionArea>
                                         <CardMedia
                                         component="img"
-                                        height='auto'
-                                        image={p2}
-                                        alt="project 2"
+                                        height='190px'
+                                        image={p5}
+                                        alt="project 5"
                                         />
                                         <CardContent>
                                         <Typography gutterBottom variant="h5" component="div" sx={{fontWeight: 600}}>
-                                          The number of people at the Sys Lab
+                                            The number of people at the Sys Lab
                                         </Typography>
                                         <Typography variant="body2" color="text.secondary">
-                                          It took 5 people to create a web application that reads the number of people from the camera to the club room over half a year.Responsible for backend development and deployment.University festival highest award work.
+                                            It took 5 people to create a web application that reads the number of people from the camera to the club room over half a year.Responsible for backend development and deployment.University festival highest award work.
                                         </Typography>
                                         </CardContent>
                                     </CardActionArea>
                                     <CardActions>
                                         <Button size="small" sx={{color: '#dda73b'}}>
-                                        <Link href="https://set1.ie.aitech.ac.jp/koukaten2021/" target="_blank" underline="none" color="inherit">Live Demo
+                                        <Link href="https://set1.ie.aitech.ac.jp/koukaten2021/" target="_blank" underline="none" color="inherit">Github Link
                                         </Link>
 
                                         </Button>
@@ -164,7 +165,7 @@ const Works: React.FC = () => {
                                 </Card>
                             </Box>
                         </Grid>
-                        <Grid item xs={6} md={4}>
+                        {/* <Grid item xs={6} md={4}>
                             <Box>
                                 <Card sx={{ maxWidth: 345 }}>
                                     <CardActionArea>
